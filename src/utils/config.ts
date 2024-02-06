9f9475af38b092ebf48f389496b517d2a1ce2d54
Falla Passeig <fallapasseig@fallapasseig.es>
2024-02-06 22:58:18 +0100

    Initial commit

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -86,7 +86,7 @@ const getSite = () => {
   const _default = {
     name: DEFAULT_SITE_NAME,
     site: undefined,
-    base: '/',
+    base: '/web',
     trailingSlash: false,
 
     googleSiteVerificationId: '',
