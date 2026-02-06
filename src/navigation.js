2581ec9787fb98b5a913413c014e4b318f248e3c
Antonio Bustos <antoniobusrod@gmail.com>
2026-02-06 14:43:27 +0100

    More updates

--- src/navigation.js
+++ src/navigation.js
@@ -1,41 +1,5 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
-
 export const headerData = {
-  links: [
-    {
-      text: 'Contingut',
-      links: [
-        {
-          text: 'Llibret 2025',
-          href: getPermalink('/llibret-2025'),
-        },
-        {
-          text: 'Turisme 2025 valencià',
-          href: getPermalink('/turisme-2025-va'),
-        },
-        {
-          text: 'Turisme 2025 castellà',
-          href: getPermalink('/turisme-2025-es'),
-        },
-        {
-          text: 'Turisme 2025 anglès',
-          href: getPermalink('/turisme-2025-en'),
-        },
-        {
-          text: 'Turisme 2025 francès',
-          href: getPermalink('/turisme-2025-fr'),
-        },
-        {
-          text: 'Turisme 2025 alemany',
-          href: getPermalink('/turisme-2025-de'),
-        },
-        {
-          text: 'Llibret 2024',
-          href: getPermalink('/llibret-2024'),
-        },
-      ],
-    },
-  ],
+  links: [],
 };
 
 export const footerData = {
@@ -47,7 +11,5 @@ export const footerData = {
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/FallaPasseigTavernes/' },
     // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
-  footNote: `
-    Tots els drets reservats.
-  `,
+  footNote: `Copyright &copy; 2026 Falla Passeig – Tots els drets reservats.`,
 };
