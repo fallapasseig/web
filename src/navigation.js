8b1ca8ec90954d7a50c8d9d84a159dbbca32a492
Antonio Bustos <antoniobusrod@gmail.com>
2026-02-06 15:45:15 +0100

    More updates

--- src/navigation.js
+++ src/navigation.js
@@ -1,5 +1,17 @@
 export const headerData = {
-  links: [],
+  links: [
+    {
+      text: 'Representants',
+      href: '/web/representants',
+
+      icon: 'tabler:users',
+    },
+    { text: 'Llibrets', href: '/web/llibrets' },
+    { text: 'Història', href: '/web/historia' },
+    { text: 'Programació 2026', href: '/web/programacio-2026' },
+    { text: 'ODS', href: '/web/ods' },
+    { text: 'Turisme', href: '/web/turisme' },
+  ],
 };
 
 export const footerData = {
@@ -9,7 +21,7 @@ export const footerData = {
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://twitter.com/FallaPasseig' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/fallapasseigtavernes/' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/FallaPasseigTavernes/' },
-    // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
+  location: `📍 Carrer Nou 32, 46760 Tavernes de la Valldigna`,
   footNote: `Copyright &copy; 2026 Falla Passeig – Tots els drets reservats.`,
 };
